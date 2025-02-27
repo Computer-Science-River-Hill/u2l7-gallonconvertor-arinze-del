47c78d198ecb37c473e558c1c3889926c326ac49
arinze-del <atc.agwuna@gmail.com>
2025-02-27 15:17:56 +0000

    Gallon converter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,19 @@
 package org.example; //DO NOT DELETE
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of gallons: ");
+        double gallons = scanner.nextDouble();
+        double quarts = gallons * 4.0;
+        double pints = quarts * 2.0;
+        double cups = pints * 2.0;
+        double tablespoons = cups *16.0;
+        System.out.println("In " + gallons + " gallons there are:");
+        System.out.println(quarts + " quarts");
+        System.out.println(pints + " pints");
+        System.out.println(cups + " cups");
+        System.out.println(tablespoons + " tablespoons");
+        scanner.close();
+    }
+}
